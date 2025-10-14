2a7af2099a63ba5f6ce54fc831cca242090c6b05
lzzheng <lzzheng@cn.ibm.com>
2025-10-14 15:26:35 +0800

    format

--- django_drf_keycloak_auth_example/wsgi.py
+++ django_drf_keycloak_auth_example/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_drf_keycloak_auth_example.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "django_drf_keycloak_auth_example.settings"
+)
 
 application = get_wsgi_application()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from django-drf-keycloak-auth-example!")
-
-
-if __name__ == "__main__":
-    main()
